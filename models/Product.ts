01f6913d456ccca3f8af6c02f75c365687fff696
john <john.davis.2015.03@gmail.com>
2022-06-06 20:47:45 -0500

    Fin de la sección 13

--- models/Product.ts
+++ models/Product.ts
@@ -38,6 +38,7 @@ const productSchema = new Schema(
 );
 
 // TODO: Crear indice de Mongo
+productSchema.index({ title: "text", tags: "text" });
 
 const Product: Model<IProduct> =
   mongoose.models.Product || model("Product", productSchema);
